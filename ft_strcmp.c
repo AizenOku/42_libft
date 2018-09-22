44e9e2ef486e77888bf85f770b178b346e242d1c
Izen Huang <ihuang@e1z4r13p1.42.us.org>
2018-09-21 23:10:14 -0700

    modified strcmp to be shorter and more concise

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,35 +6,16 @@
 /*   By: ihuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 17:51:17 by ihuang            #+#    #+#             */
-/*   Updated: 2018/09/13 22:53:23 by ihuang           ###   ########.fr       */
+/*   Updated: 2018/09/21 23:09:50 by ihuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	int				i;
-	unsigned char	c1;
-	unsigned char	c2;
-
-	i = 0;
-	while (s1[i] && s2[i])
+	while (*s1 == *s2)
 	{
-		c1 = s1[i];
-		c2 = s2[i];
-		if (c1 != c2)
-			return (c1 - c2);
-		i++;
+		s1++;
+		s2++;
 	}
-	if (s1[i])
-	{
-		c1 = s1[i];
-		return (c1);
-	}
-	else if (s2[i])
-	{
-		c2 = s2[i];
-		return (0 - c2);
-	}
-	else
-		return (0);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
